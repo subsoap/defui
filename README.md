266442a8fe1ef1575167fa0d88a42b0f14637787
Brian Kramer <subsoap@gmail.com>
2017-08-12 15:15:05 -0700

    Setting up

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-# defui
-Fork of dirtylarry GUI library with extra themes
+# DefUI
+Fork of dirtylarry GUI library with extra themes and functionality. This is a hard fork which is not meant to be compatible with dirtylarry, but projects can be adapted and content could be added from this to dirtylarry library.
+
+# TODO
+* make themes better to add
+* need to disable hover / input on other UI while touch click is held down start on another - take/releas focus
+* color picker
+* circular progress bar
+* alerts
+* modal popup
+* knobs
+* progress bar
+* option list
+* rgb slider
+* infinite loop scroller
+* toolbar
+* tab bar
+* tooltips
+* scrollbars
+* audio volume slider
+* notched slider (snap to notches)
+* momentum / friction / drag and click threshold for scrollers
+* accelerated backspace not relying on game.project input repeat?
+* clicking into text boxes to change pointer
+* selecting areas of text box with click drag or holding shift plus arrow keys
+* panels within panels
+
+# Usage
+1. Add latest zip URL as a [dependency](http://www.defold.com/manuals/libraries/#_setting_up_library_dependencies) in your Defold project: `https://github.com/subsoap/defui/archive/master.zip`
+
+2. Add one of the supplied GUI template files, located in `defui/*.gui`, to your GUI scene.
+
+3. In your GUI script, add `local defui = require "defui/defui"` to the top and make sure the script has [acquired input focus](http://www.defold.com/manuals/input/#_acquiring_and_releasing_input_focus).
+
+4. Then add the corresponding function inside your `on_input`:
+    * `defui/input.gui`:
+    ```Lua
+    defui:input("node_id", action_id, action, gui.KEYBOARD_TYPE_DEFAULT, "Default text")
+    ```
+    * `defui/button.gui`:
+    ```Lua
+    defui:button("node_id", action_id, action, function ()
+        print("button was pressed")
+    end)
+    ```
+    * `defui/checkbox*.gui`:
+    ```Lua
+    self.checkbox_value = defui:checkbox("node_id", action_id, action, self.checkbox_value)
+    ```
+    * `defui/radio*.gui`:
+    ```Lua
+    self.radio_value = defui:radio("node_a_id", action_id, action, "a", self.radio_value)
+    self.radio_value = defui:radio("node_b_id", action_id, action, "b", self.radio_value)
+    ```
+
+5. In the `input/game.input_binding`, add the following triggers:
+   * *Key Triggers*: Input: `key-backspace`, Action: `backspace`
+   * *Mouse Triggers*: Input: `mouse-button-1`, Action: `touch`
+   * *Text Triggers*: Input: `text`, Action: `text`
+   * *Text Triggers*: Input: `marked-text`, Action: `marked_text`
+
+The backspace delay is configured in game.project under "Input", the values are "Repeat Delay" and "Repeat Interval".
+   
+For more usage examples take a look at [main/examples.gui_script](main/examples.gui_script) and [defui/defui.lua](defui/defui.lua) for configurational options.
+
+## Examples
+* Using the built in font:
+![Example project with Defold default font](doc/example_vera_font.png)
+* Using [Input](http://input.fontbureau.com/) font:
+![Example project with Input font](doc/example_input_font.png)
---
+++ defui/defui.lua
@@ -0,0 +1,322 @@
+local defui = {}
+
+defui.action_id_touch       = hash("touch")
+defui.action_id_text        = hash("text")
+defui.action_id_marked_text = hash("marked_text")
+defui.action_id_backspace   = hash("backspace")
+
+defui.active_input_marked = ""
+defui.active_node = nil
+defui.input_nodes = {}
+defui.colors = {
+    base = vmath.vector4(1,1,1,1),
+    active = vmath.vector4(1,1,1,1),
+    enabled = vmath.vector4(1,1,1,1),
+    disabled = vmath.vector4(1,1,1,0.2),
+    mark = vmath.vector4(255/255,40/255,40/255,1)
+}
+defui.utf8_gfind = "([%z\1-\127\194-\244][\128-\191]*)"
+
+
+function defui.is_enabled(self, node)
+    local parent = gui.get_parent(node)
+    if parent then
+        return defui.is_enabled(self, parent)
+    end
+    return gui.is_enabled(node)
+end
+
+local function safe_get_node(node)
+	if pcall(function() gui.get_node(node) end) then
+		return gui.get_node(node)
+	else
+		return nil
+	end
+end
+
+local function hit_test(self, node, action_id, action)
+    if not defui.is_enabled(self, node) then
+        return false
+    end
+
+    local hit = gui.pick_node( node, action.x, action.y )
+    local touch = action_id == defui.action_id_touch
+    return touch and hit
+end
+
+function defui.hit(self, node, action_id, action, cb)
+    node = type(node) == "string" and gui.get_node(node) or node
+    local hit = hit_test(self, node, action_id, action)
+    if hit and action.released then
+        cb()
+    end
+    return hit
+end
+
+
+function defui.button(self, node, action_id, action, cb)
+
+    local node_bg = gui.get_node(node .. "/larrybutton")
+    local node_label = gui.get_node(node .. "/larrylabel")
+
+    local label_p = vmath.vector3(0.0)
+    local flipbook = "button_normal"
+    local hit = hit_test(self, node_bg, action_id, action)
+    if hit then
+        if action.released then
+            cb()
+        else
+            label_p.y = -2.0
+            flipbook = "button_pressed"
+        end
+    end
+
+    gui.play_flipbook(node_bg, flipbook)
+    gui.set_position(node_label, label_p)
+
+end
+
+
+function defui.checkbox(self, node, action_id, action, value)
+
+    local checked = value
+
+    local node_bg = gui.get_node(node .. "/larrycheckbox")
+    local node_label = gui.get_node(node .. "/larrylabel")
+
+    local hit = hit_test(self, node_bg, action_id, action) or hit_test(self, node_label, action_id, action)
+    if hit and action.released then
+        checked = not checked
+    end
+
+    local flipbook = "checkbox_" .. (checked and "checked_" or "") .. (hit and not action.released and "pressed" or "normal")
+    gui.play_flipbook(node_bg, flipbook)
+    return checked
+end
+
+function defui.radio(self, node, action_id, action, id, value)
+
+    local node_bg = gui.get_node(node .. "/larryradio")
+    local node_label = gui.get_node(node .. "/larrylabel")
+
+    local hit = hit_test(self, node_bg, action_id, action) or hit_test(self, node_label, action_id, action)
+    if hit and action.released then
+        value = id
+    end
+
+    local flipbook = "radio_" .. (value == id and "checked_" or "") .. (hit and not action.released and "pressed" or "normal")
+    gui.play_flipbook(node_bg, flipbook)
+    return value
+end
+
+function defui.input(self, node, action_id, action, type, empty_text)
+
+    local node_bg = gui.get_node(node .. "/bg")
+    local node_inner = gui.get_node(node .. "/inner")
+    local node_content = gui.get_node(node .. "/content")
+    local node_cursor = gui.get_node(node .. "/cursor")
+
+    -- create a key that is unique to the gui scene
+    local url = msg.url()
+    local key = tostring(url.socket) .. hash_to_hex(url.path) .. hash_to_hex(url.fragment or hash("")) .. node
+
+    -- create entry in input_nodes table on first call
+    if (not defui.input_nodes[key]) then
+        defui.input_nodes[key] = { id = node, data = gui.get_text(node_content), active = false }
+    end
+
+    local input_node = defui.input_nodes[key]
+    if not defui.is_enabled(self, node_bg) then
+        return input_node.data
+    end
+
+    -- if we don't have an active node or the active node is other than the current node and
+    -- the current node was flagged as active then we need to deactivate it
+    -- note that we need to deactivate the previously active input node in the correct gui_script
+    -- otherwise we will get the "node used in the wrong scene" error
+    if (not defui.active_node or defui.active_node ~= input_node) and input_node.active then
+        local active_node_bg = gui.get_node(input_node.id .. "/bg")
+        local active_node_cursor = gui.get_node(input_node.id .. "/cursor")
+        input_node.active = false
+        gui.cancel_animation(active_node_bg, "color")
+        gui.animate(active_node_bg, "color", defui.colors.base, gui.EASING_OUTCUBIC, 0.2)
+        gui.cancel_animation(active_node_cursor, "color")
+        gui.animate(active_node_cursor, "size", vmath.vector3(0, 48, 0), gui.EASING_OUTCUBIC, 0.1)
+    end
+
+    -- input/output states
+    local text_output = input_node.data
+    local text_marked = ""
+    local touch = action_id == defui.action_id_touch
+
+    -- set inner box (clipper) to inner size of input field
+    local s = gui.get_size(node_bg)
+    s.x = s.x - 4-32
+    s.y = s.y - 4
+    gui.set_size(node_inner, s)
+
+    -- switch active input node
+    if hit_test(self, node_bg, action_id, action) then
+            -- change to new entry
+            gui.reset_keyboard()
+            defui.active_input_marked = ""
+            defui.active_node = input_node
+            defui.active_node.active = true
+            gui.animate(node_bg, "color", defui.colors.active, gui.EASING_OUTCUBIC, 0.2)
+            gui.animate(node_cursor, "size", vmath.vector3(4, 32, 0), gui.EASING_OUTCUBIC, 0.2)
+
+            -- show keyboard for mobile devices
+            gui.show_keyboard(type, true)
+    end
+
+    -- handle new input if current input node is active
+    if (defui.active_node == input_node) then
+
+        -- new raw text input
+        if (action_id == defui.action_id_text) then
+            defui.active_node.data = defui.active_node.data .. action.text
+            defui.active_input_marked = ""
+
+        -- new marked text input (uncommitted text)
+        elseif (action_id == defui.action_id_marked_text) then
+            defui.active_input_marked = action.text
+
+        -- input deletion
+        elseif (action_id == defui.action_id_backspace and (action.pressed or action.repeated)) then
+            local last_s = 0
+            for uchar in string.gfind(defui.active_node.data, defui.utf8_gfind) do
+              last_s = string.len(uchar)
+            end
+
+            defui.active_node.data = string.sub(defui.active_node.data, 1, string.len(defui.active_node.data) - last_s)
+        end
+
+        -- set text color
+        gui.set_color(node_content, defui.colors.enabled)
+
+        -- if current input is active, include marked text in output
+        text_output = defui.active_node.data .. defui.active_input_marked
+
+        -- get text metrics for both raw input data and marked text
+        local m_t = gui.get_text_metrics(gui.get_font(node_content), defui.active_node.data, 0, false, 0, 0)
+        local m_m = gui.get_text_metrics(gui.get_font(node_content), defui.active_input_marked, 0, false, 0, 0)
+
+        -- set cursor (and marked text bg)
+        gui.set_position(node_cursor, vmath.vector3(4 + m_t.width, 0, 0))
+        if (defui.active_input_marked and #defui.active_input_marked) then
+            gui.animate(node_cursor, "size", vmath.vector3(4 + m_m.width, 32, 0), gui.EASING_OUTCUBIC, 0.2)
+        else
+            gui.animate(node_cursor, "size", vmath.vector3(4, 32, 0), gui.EASING_OUTCUBIC, 0.2)
+        end
+
+    end
+
+    -- if input field is a password, mask the text output
+    if (type == gui.KEYBOARD_TYPE_PASSWORD) then
+        local masked_text = ""
+        for uchar in string.gfind(text_output, defui.utf8_gfind) do
+          masked_text = masked_text .. "*"
+        end
+        text_output = masked_text
+    end
+
+    gui.set_text(node_content, text_output)
+
+    -- show grayed out label/text if input is empty
+    if (empty_text and string.len(text_output) == 0) then
+        gui.set_color(node_content, defui.colors.disabled)
+        gui.set_text(node_content, empty_text)
+    end
+
+    return input_node.data
+end
+
+function defui.scrollarea(self, node_str, action_id, action, scroll, cb)
+
+    local node = gui.get_node(node_str)
+    local parent = gui.get_parent(node)
+    local touch = action_id == defui.action_id_touch
+    
+    local scroll = scroll
+    if not scroll then
+    	-- assume initial call
+    	local p = gui.get_position(node)
+    	local s = gui.get_size(node)
+    	scroll = {drag=false,started=false,dx=0,dy=0,ox=p.x,oy=p.y,ow=s.x,oh=s.y}
+    	
+    	scroll.bar_x = safe_get_node(node_str .. "_barx")
+    	scroll.bar_y = safe_get_node(node_str .. "_bary")
+    end
+    
+    local hit = false
+	if parent then
+		hit = hit_test(self, parent, action_id, action)
+	else
+		hit = hit_test(self, node, action_id, action)
+	end
+    
+    local consumed_input = false
+	if touch then
+	    
+	    -- end scroll/drag
+    	if scroll.drag and action.released then
+    		scroll.drag = false
+    		scroll.started = false
+    		consumed_input = true
+    		
+    	-- potentially start scroll/drag
+    	elseif hit and action.pressed then
+    		scroll.started = true
+    		
+    	-- start scroll/drag
+    	elseif scroll.started and hit and (action.dx ~= 0 or action.dy ~= 0) then
+    		scroll.drag = true
+    		scroll.started = false
+    		consumed_input = true
+    	end
+    	
+    	if scroll.drag then
+	    	consumed_input = true
+	
+    		scroll.dx = scroll.dx - action.dx
+    		scroll.dy = scroll.dy + action.dy
+    		
+    		if parent then
+    			local s = gui.get_size(parent)
+	    		local min_x = 0
+	    		local min_y = 0
+    			local max_x = math.max(0, scroll.ow - s.x)
+    			local max_y = math.max(0, scroll.oh - s.y)
+    			
+    			if scroll.dx < min_x then scroll.dx = min_x end	
+	    		if scroll.dx > max_x then scroll.dx = max_x end
+	    		if scroll.dy < min_y then scroll.dy = min_y end
+	    		if scroll.dy > max_y then scroll.dy = max_y end
+	    		
+	    		if scroll.bar_x and max_x > 0 then
+	    			local delta_x = scroll.dx / max_x
+	    			local bar_s = gui.get_size(scroll.bar_x)
+	    			local p = vmath.vector3((s.x-bar_s.x) * delta_x, -s.y, 0)
+	    			gui.set_position(scroll.bar_x, p)
+	    		end
+	    		if scroll.bar_y and max_y > 0 then
+	    			local delta_y = scroll.dy / max_y
+	    			local bar_s = gui.get_size(scroll.bar_y)
+	    			local p = vmath.vector3(s.x, -(s.y-bar_s.y) * delta_y, 0)
+	    			gui.set_position(scroll.bar_y, p)
+	    		end
+    		end
+    		
+    		gui.set_position(node, vmath.vector3(scroll.ox-scroll.dx, scroll.oy+scroll.dy, 0))
+	    	
+    	end
+    end
+	    
+	if not consumed_input and ((touch and hit) or not touch) then
+    	cb(self, action_id, action)
+    end
+
+	return scroll
+end
+
+return defui
